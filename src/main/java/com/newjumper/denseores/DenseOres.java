ea22233fd647edbd6284aea7b0b26b7f41787adb
muhta <muhtasim.nabi@gmail.com>
2023-02-05 19:14:40 -0600

    version 1.2.0 for MC 1.19.2

--- src/main/java/com/newjumper/denseores/DenseOres.java
+++ src/main/java/com/newjumper/denseores/DenseOres.java
@@ -1,9 +1,19 @@
 package com.newjumper.denseores;
 
 import com.newjumper.denseores.content.DenseBlocks;
+import com.newjumper.denseores.datagen.assets.DenseOresBlockStateProvider;
+import com.newjumper.denseores.datagen.assets.DenseOresItemModelProvider;
+import com.newjumper.denseores.datagen.assets.ENLanguageProvider;
+import com.newjumper.denseores.datagen.data.DenseOresBlockTagsProvider;
+import com.newjumper.denseores.datagen.data.DenseOresItemTagsProvider;
+import com.newjumper.denseores.datagen.data.DenseOresLootTableProvider;
+import com.newjumper.denseores.datagen.data.SmeltingRecipesProvider;
 import com.newjumper.denseores.world.DenseConfiguredFeatures;
 import com.newjumper.denseores.world.DensePlacedFeatures;
+import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -20,6 +30,25 @@ public class DenseOres {
         DensePlacedFeatures.PLACED_FEATURES.register(eventBus);
         DenseBlocks.ITEMS.register(eventBus);
 
+        eventBus.addListener(this::generateData);
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public void generateData(final GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        ExistingFileHelper fileHelper = event.getExistingFileHelper();
+
+        // assets
+        generator.addProvider(event.includeClient(), new DenseOresBlockStateProvider(generator, fileHelper));
+        generator.addProvider(event.includeClient(), new DenseOresItemModelProvider(generator, fileHelper));
+        generator.addProvider(event.includeClient(), new ENLanguageProvider(generator));
+
+        // data
+        DenseOresBlockTagsProvider blockTags = new DenseOresBlockTagsProvider(generator, fileHelper);
+        generator.addProvider(event.includeServer(), blockTags);
+        generator.addProvider(event.includeServer(), new DenseOresItemTagsProvider(generator, blockTags, fileHelper));
+
+        generator.addProvider(event.includeServer(), new DenseOresLootTableProvider(generator));
+        generator.addProvider(event.includeServer(), new SmeltingRecipesProvider(generator));
     }
 }
