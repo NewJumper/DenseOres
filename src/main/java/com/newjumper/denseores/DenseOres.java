84ef617a917d31876dffd28fc71c5289e2452113
muhta <muhtasim.nabi@gmail.com>
2023-06-16 16:44:20 -0500

    creative tab fixes and loot table datagen cleanup

--- src/main/java/com/newjumper/denseores/DenseOres.java
+++ src/main/java/com/newjumper/denseores/DenseOres.java
@@ -5,60 +5,43 @@ import com.newjumper.denseores.datagen.assets.DenseOresBlockStateProvider;
 import com.newjumper.denseores.datagen.assets.DenseOresItemModelProvider;
 import com.newjumper.denseores.datagen.assets.ENLanguageProvider;
 import com.newjumper.denseores.datagen.data.*;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
-import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(DenseOres.MOD_ID)
 public class DenseOres {
     public static final String MOD_ID = "denseores";
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
 
     public DenseOres() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         DenseBlocks.BLOCKS.register(eventBus);
         DenseBlocks.ITEMS.register(eventBus);
+        CREATIVE_MODE_TABS.register(eventBus);
 
-        eventBus.addListener(this::registerCreativeTab);
         eventBus.addListener(this::generateData);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    public void registerCreativeTab(final CreativeModeTabEvent.Register event) {
-        event.registerCreativeModeTab(new ResourceLocation(MOD_ID, "denseores"), builder -> builder.icon(() -> new ItemStack(DenseBlocks.DENSE_DIAMOND_ORE.get()))
-                .title(Component.translatable("itemGroup." + MOD_ID))
-                .displayItems((itemDisplay, output) -> {
-                    output.accept(DenseBlocks.DENSE_COAL_ORE.get());
-                    output.accept(DenseBlocks.DENSE_DEEPSLATE_COAL_ORE.get());
-                    output.accept(DenseBlocks.DENSE_IRON_ORE.get());
-                    output.accept(DenseBlocks.DENSE_DEEPSLATE_IRON_ORE.get());
-                    output.accept(DenseBlocks.DENSE_COPPER_ORE.get());
-                    output.accept(DenseBlocks.DENSE_DEEPSLATE_COPPER_ORE.get());
-                    output.accept(DenseBlocks.DENSE_GOLD_ORE.get());
-                    output.accept(DenseBlocks.DENSE_DEEPSLATE_GOLD_ORE.get());
-                    output.accept(DenseBlocks.DENSE_REDSTONE_ORE.get());
-                    output.accept(DenseBlocks.DENSE_DEEPSLATE_REDSTONE_ORE.get());
-                    output.accept(DenseBlocks.DENSE_EMERALD_ORE.get());
-                    output.accept(DenseBlocks.DENSE_DEEPSLATE_EMERALD_ORE.get());
-                    output.accept(DenseBlocks.DENSE_LAPIS_ORE.get());
-                    output.accept(DenseBlocks.DENSE_DEEPSLATE_LAPIS_ORE.get());
-                    output.accept(DenseBlocks.DENSE_DIAMOND_ORE.get());
-                    output.accept(DenseBlocks.DENSE_DEEPSLATE_DIAMOND_ORE.get());
-                    output.accept(DenseBlocks.DENSE_NETHER_GOLD_ORE.get());
-                    output.accept(DenseBlocks.DENSE_NETHER_QUARTZ_ORE.get());
-                    output.accept(DenseBlocks.ANCIENT_NETHER_ORE.get());
-                })
-        );
-    }
+    public static final RegistryObject<CreativeModeTab> denseOresTab = CREATIVE_MODE_TABS.register("dense_ores_tab", () -> CreativeModeTab.builder()
+            .icon(() -> new ItemStack(DenseBlocks.DENSE_DIAMOND_ORE.get()))
+            .title(Component.translatable("itemGroup." + MOD_ID))
+            .displayItems((itemDisplay, output) -> DenseBlocks.BLOCKS.getEntries().forEach(block -> output.accept(block.get()))).build()
+    );
 
     public void generateData(final GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
--- src/main/java/com/newjumper/denseores/datagen/data/DenseOresLootTableProvider.java
+++ src/main/java/com/newjumper/denseores/datagen/data/DenseOresLootTableProvider.java
@@ -2,31 +2,13 @@ package com.newjumper.denseores.datagen.data;
 
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.LootTables;
-import net.minecraft.world.level.storage.loot.ValidationContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Map;
 
 public class DenseOresLootTableProvider extends LootTableProvider {
-    private final List<SubProviderEntry> lootTables = List.of(new LootTableProvider.SubProviderEntry(DenseOresLootTables::new, LootContextParamSets.BLOCK));
-
     public DenseOresLootTableProvider(PackOutput output) {
         super(output, BuiltInLootTables.all(), List.of(new LootTableProvider.SubProviderEntry(DenseOresLootTables::new, LootContextParamSets.BLOCK)));
-    }
-
-    @Override
-    public @NotNull List<SubProviderEntry> getTables() {
-        return lootTables;
-    }
-
-    @Override
-    protected void validate(Map<ResourceLocation, LootTable> map, @NotNull ValidationContext validationcontext) {
-        map.forEach((id, table) -> LootTables.validate(validationcontext, id, table));
     }
 }
