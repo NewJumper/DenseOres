11e15348a48f789e285892521d816521cecc4c89
NewJumper <muhtasim.nabi@gmail.com>
2023-12-29 15:13:43 -0600

    improved code stuff

--- src/main/java/com/newjumper/denseores/datagen/data/DenseOresLootTableProvider.java
+++ src/main/java/com/newjumper/denseores/datagen/data/DenseOresLootTableProvider.java
@@ -2,13 +2,21 @@ package com.newjumper.denseores.datagen.data;
 
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.storage.loot.*;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Map;
 
 public class DenseOresLootTableProvider extends LootTableProvider {
-    public DenseOresLootTableProvider(PackOutput output) {
-        super(output, BuiltInLootTables.all(), List.of(new LootTableProvider.SubProviderEntry(DenseOresLootTables::new, LootContextParamSets.BLOCK)));
+    public DenseOresLootTableProvider(PackOutput pOutput) {
+        super(pOutput, BuiltInLootTables.all(), List.of(new LootTableProvider.SubProviderEntry(DenseOresBlockLoot::new, LootContextParamSets.BLOCK)));
+    }
+
+    @Override
+    protected void validate(Map<ResourceLocation, LootTable> map, @NotNull ValidationContext context) {
+        map.forEach((id, table) -> table.validate(context.setParams(table.getParamSet()).enterElement("{" + id + "}", new LootDataId<>(LootDataType.TABLE, id))));
     }
 }
